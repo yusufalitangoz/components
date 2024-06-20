23312c46fbcb5b66fa27c79c83651890fd733185
yali <tangozyusufali@gmail.com>
2024-06-21 00:43:59 +0300

    feat: header

--- app.vue
+++ app.vue
@@ -1,3 +1,5 @@
 <template>
-  <main></main>
+  <main>
+    <Header />
+  </main>
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="container mt-5 flex justify-between">
+    <h1 class="text-sm font-bold">yusufalitangoz/components</h1>
+    <ExamplesColorModeToggle />
+  </header>
+</template>
