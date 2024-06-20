0bf2fa8142cae690e5e80ddc16901cd433c489cc
yali <tangozyusufali@gmail.com>
2024-06-21 00:43:53 +0300

    feat: color mode toggle

---
+++ components/examples/ColorModeToggle.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const icon = new Map([
+  ["system", "eva:monitor-outline"],
+  ["dark", "akar-icons:moon"],
+  ["light", "ph:sun"],
+]);
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button>
+        <Icon :name="icon.get(colorMode.preference)!" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuItem @click="colorMode.preference = 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'dark'">
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'system'">
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
