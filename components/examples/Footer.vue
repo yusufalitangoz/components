c5134134586fbcc3f9d54c32d5721ae9e4087c69
yali <tangozyusufali@gmail.com>
2024-06-25 02:37:44 +0300

    style(footer): add container

--- components/examples/Footer.vue
+++ components/examples/Footer.vue
@@ -40,33 +40,33 @@ const socials = [
 </script>
 
 <template>
-  <footer
-    class="bg-zinc-100 dark:bg-black border-t-[1px] w-full p-10 grid md:grid-cols-2 gap-10"
-  >
-    <section class="flex flex-col gap-2 place-items-start">
-      <ExamplesLogo />
-      <p class="text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, in
-        exercitationem.
-      </p>
-      <section class="flex flex-wrap gap-2">
-        <NuxtLink
-          v-for="social in socials"
-          :key="social.icon"
-          :to="social.href"
-          target="_blank"
-        >
-          <Icon :name="social.icon" size="17" />
-        </NuxtLink>
+  <footer class="bg-zinc-100 dark:bg-black border-t-[1px] w-full">
+    <section class="container p-10 grid md:grid-cols-2 gap-10">
+      <section class="flex flex-col gap-2 place-items-start">
+        <ExamplesLogo />
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, in
+          exercitationem.
+        </p>
+        <section class="flex flex-wrap gap-2">
+          <NuxtLink
+            v-for="social in socials"
+            :key="social.icon"
+            :to="social.href"
+            target="_blank"
+          >
+            <Icon :name="social.icon" size="17" />
+          </NuxtLink>
+        </section>
       </section>
-    </section>
-    <section class="flex flex-wrap gap-10 md:justify-end">
-      <ul v-for="group in linkGroups" :key="group.title">
-        <h1 class="font-semibold mb-2">{{ group.title }}</h1>
-        <li v-for="link in group.links" :key="link.name" class="text-sm">
-          <NuxtLink :to="link.url">{{ link.name }}</NuxtLink>
-        </li>
-      </ul>
+      <section class="flex flex-wrap gap-10 md:justify-end">
+        <ul v-for="group in linkGroups" :key="group.title">
+          <h1 class="font-semibold mb-2">{{ group.title }}</h1>
+          <li v-for="link in group.links" :key="link.name" class="text-sm">
+            <NuxtLink :to="link.url">{{ link.name }}</NuxtLink>
+          </li>
+        </ul>
+      </section>
     </section>
   </footer>
 </template>
